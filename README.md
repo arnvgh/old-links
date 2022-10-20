6cca44cd1a645bd509a774dce6679965318eec40
Arunava Ghosh <102473837+its-ag@users.noreply.github.com>
2022-10-20 23:21:15 +0530

    feat: init

---
+++ README.md
@@ -0,0 +1,29 @@
+# LinkZia 🔗 - A Better Links Manager 
+(Idk how this name came to my mind :P)
+
+![linkzia](https://user-images.githubusercontent.com/102473837/197008496-60dc8285-8416-4118-a89e-e90e210b31e8.png)
+
+### LinkZia is a links manager 🔗 , built with the [Astro](https://astro.build) and [Tailwind CSS](https://tailwindcss.com/) .
+
+
+# Ussage
+
+
+```bash
+git clone https://github.com/its-ag/linkzia.git
+yarn install
+yarn run dev
+```
+
+- Go to ```src/data.ts``` and add/update the arrays with your own details.
+- I have used [Astro Icon](https://www.npmjs.com/package/astro-icon) which uses [Iconify](https://iconify.design/) for icons, you can use any other svg icon library.
+
+# Cloning/ Forking  
+
+- Please do not remove the credit to me, Arunava Ghosh, in the footer.
+- Give it a star if you like it ⭐
+
+# Author
+
+- [Arunava](https://arunava.tech)
+- [Twitter](https://twitter.com/ag_arunava)
---
+++ src/data.ts
@@ -0,0 +1,67 @@
+export const profile = {
+  avatar: `/assets/profile.png`,
+  name: `Arunava Ghosh`,
+  bio: `Just a guy that likes to code..`,
+};
+
+export const header = [
+  {
+    text: "Check Out My Website",
+    url: "https://arunava.tech",
+    icon: "https://cdn.bio.link/uploads/thumbnails/2022-10-15/vqVkK9AaFpxuLCl0ploaded_O8qyJM0N.gif",
+  },
+  {
+    text: "Read My @Latest Post",
+    url: "https://www.arunava.tech/blog/how-to-learn-to-code",
+    icon: "https://cdn.bio.link/uploads/thumbnails/2022-10-15/ZlYwYVAhMQ1zeUvrploaded_TPoppaaP.gif",
+  },
+];
+
+export const socials = [
+  {
+    title: "Mail",
+    icon: "ant-design:mail-outlined",
+    url: "mailto:itsag0024@gmail.com",
+  },
+  {
+    title: "GitHub",
+    icon: "akar-icons:github-outline-fill",
+    url: "https://github.com/its-ag",
+  },
+  {
+    title: "Spotify",
+    icon: "teenyicons:spotify-outline",
+    url: "https://open.spotify.com/user/zfu9cur8fpnw6oc4q8vm55op6?si=47bde3535c5f4d09",
+  },
+  {
+    title: "Twitter",
+    icon: "mdi:twitter",
+    url: "https://twitter.com/ag_arunava",
+  },
+  {
+    title: "Instagram",
+    icon: "akar-icons:instagram-fill",
+    url: "https://instagram.com/ag_arunava",
+  },
+  {
+    title: "Discord",
+    icon: "fa-brands:discord",
+    url: "https://discord.com/users/968448572641124352",
+  },
+  {
+    title: "Substack",
+    icon: "simple-icons:substack",
+    url: "https://arunavaghosh.substack.com/",
+  },
+  {
+    title: "Telegram",
+    icon: "akar-icons:telegram-fill",
+    url: "https://telegram.me/ag_arunava",
+  },
+  // Add more socials here, get 'icon' from https://iconify.design/icon-sets/
+  // {
+  //   title: 'Dev.to',
+  //   icon: 'mdi:dev-to',
+  //   url: 'https://dev.to/',
+  // },
+];
